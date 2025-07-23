e12e4ac8cb2ccb8d775b0bc08588486b7822fd51
9n8e <boombloxkewl@gmail.com>
2025-07-23 17:18:37 -0400

    Create isRobloxSite.cpp

---
+++ isRobloxSite.cpp
@@ -0,0 +1,10 @@
+// my reconstruction
+namespace RBX {
+  class Http {
+    public:
+      static bool isRobloxSite(const char* url) {
+        // i have no clue wtf is going on here
+        return url == "roblox.com";
+      }
+  }
+}
