594940c86c9af178366fd9b5154b723d1cff9f3f
9n8e <boombloxkewl@gmail.com>
2025-08-18 11:48:22 -0400

    Create ThumbnailGenerator.cpp

---
+++ rbxgs/webservice/ThumbnailGenerator/ThumbnailGenerator.cpp
@@ -0,0 +1,14 @@
+void __usercall ThumbnailGenerator::ThumbnailGenerator(ThumbnailGenerator *this@<ecx>, _DWORD *a2@<esi>)
+{
+  RBX::DescribedCreatable<ThumbnailGenerator,RBX::Instance,&char const * const sThumbnailGenerator>::DescribedCreatable<ThumbnailGenerator,RBX::Instance,&char const * const sThumbnailGenerator>(this);
+  *a2 = &ThumbnailGenerator::`vftable'{for `RBX::GuidItem<RBX::Instance>'};
+  a2[4] = &ThumbnailGenerator::`vftable'{for `RBX::AbstractFactoryProduct<RBX::Instance>'};
+  a2[5] = &ThumbnailGenerator::`vftable'{for `RBX::Reflection::Described<RBX::Instance,&char const * const RBX::sInstance,RBX::Reflection::DescribedBase>'};
+  a2[8] = &ThumbnailGenerator::`vftable'{for `RBX::Debugable'};
+  a2[9] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::ChildAdded>'};
+  a2[15] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::ChildRemoved>'};
+  a2[21] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::DescendentAdded>'};
+  a2[27] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::DescendentRemoving>'};
+  a2[33] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::AncestorChanged>'};
+  a2[39] = &ThumbnailGenerator::`vftable'{for `RBX::Notifier<RBX::Instance,RBX::PropertyChanged>'};
+}
