ecf391a32bf5171a89b28af03b63a8ed9197629f
9n8e <boombloxkewl@gmail.com>
2025-08-21 16:38:33 -0400

    fixed link with intellisense

--- App/v8kernel/Link.cpp
+++ App/v8kernel/Link.cpp
@@ -1,53 +1,46 @@
+#include "v8kernel/Link.h"
+#include "v8kernel/Body.h"
+
 namespace RBX {
-    class Link {
-    protected:
-        RBX::Body* body;
-        G3D::CoordinateFrame parentCoord; // childCoord childCordInverse childInParent
-        G3D::CoordinateFrame childCoord;
-        G3D::CoordinateFrame childCoordInverse;
-        G3D::CoordinateFrame childInParent;
-        int stateIndex;
+    void Link::dirty() {
+        Body* body = this->body;
+        if (body) {
+            body->getRoot()->advanceStateIndex();
+        }
+    }
 
-        void dirty() {
-            RBX::Body body = this->body;
-            if (body) {
-                body->root->advanceStateIndex();
-            }
+    void Link::setBody(RBX::Body* body) {
+        this->body = body;
+    }
+
+    Link::Link() {
+        this->body = &Body();
+        this->parentCoord = G3D::CoordinateFrame();
+        this->childCoord = G3D::CoordinateFrame();
+        this->childCoordInverse = G3D::CoordinateFrame();
+        this->childInParent = G3D::CoordinateFrame();
+        this->stateIndex = body->getNextStateIndex();
+    }
+
+    const G3D::CoordinateFrame &Link::getChildInParent() {
+        RBX::Body* parent = this->body->getParent();
+        // parent->updatePV;
+        int stateIndex = parent->getStateIndex();
+
+        if (this->stateIndex != stateIndex) {
+            this->computeChildInParent(this->childInParent);
+            this->stateIndex = stateIndex;
+            return this->childInParent;
         }
 
-        void setBody(RBX::Body* body) {
-            this->body = body;
-        }
+        return this->childInParent;
+    }
 
-        Link* Link() {
-            this->body = NULL;
-            this->parentCoord = new G3D::CoordinateFrame;
-            this->childCoord = new G3D::CoordinateFrame;
-            this->childCoordInverse = new G3D::CoordinateFrame;
-            this->childInParent = new G3D::CoordinateFrame;
-            this->stateIndex = RBX::Body::getNextStateIndex();
-        }
+    void Link::reset(const G3D::CoordinateFrame& parentC, const G3D::CoordinateFrame& childC) {
+        G3D::CoordinateFrame result;
 
-        const G3D::CoordinateFrame getChildInParent() {
-            RBX::Body parent = this->body->parent;
-            parent->updatePV;
-            int stateIndex = parent->stateIndex;
-
-            if (this->stateIndex != stateIndex) {
-                this->computeChildInParent(this, &this->childInParent);
-                this->stateIndex = stateIndex;
-                return &this->childInParent;
-            }
-
-            return &this->childInParent;
-        }
-
-        void reset(const G3D::CoordinateFrame* parentC, G3D::CoordinateFrame* childC) {
-            G3D::CoordinateFrame result;
-
-            this->parentCoord = parentC;
-            this->childCoord = childC;
-            this->childCoordInverse = G3D::CoordinateFrame::inverse(childC, &result);
-        }
+        this->parentCoord = parentC;
+        this->childCoord = childC;
+        this->childCoordInverse = childC.inverse();
     }
 }
