b39efff054dce2b98ac6b62916bb2850092943d8
9n8e <boombloxkewl@gmail.com>
2025-08-21 17:54:15 -0400

    added definitions for RBX::Edge and added enum EdgeType

--- App/include/v8world/Edge.h
+++ App/include/v8world/Edge.h
@@ -3,7 +3,40 @@
 #include "v8world/IPipelined.h"
 
 namespace RBX {
-    class Edge : public IPipelined {};
+    class Edge : public IPipelined {
+    private: 
+        RBX::Sim::EdgeState edgeState;
+        RBX::Primitive* prim0;
+        RBX::Primitive* prim1;
+        RBX::Edge* next0;
+        RBX::Edge* next1;
+        bool inEdgeList;
+  
+    public: 
+        enum EdgeType {
+            JOINT = 0x0000,
+            CONTACT = 0x0001,
+        };
+        Edge(const RBX::Edge&);
+        Edge(RBX::Primitive*, RBX::Primitive*);
+        virtual ~Edge();
+        bool getInEdgeList() const;
+        void setInEdgeList(bool);
+        virtual RBX::Edge::EdgeType getEdgeType() const;
+        RBX::Sim::EdgeState getEdgeState() const;
+        void setEdgeState(RBX::Sim::EdgeState);
+        RBX::Primitive* getPrimitive(int32_t) const;
+        RBX::Primitive* otherPrimitive(int32_t) const;
+        RBX::Primitive* otherPrimitive(const RBX::Primitive*) const;
+        int32_t getPrimitiveId(const RBX::Primitive*) const;
+        RBX::Edge* getNext(const RBX::Primitive*) const;
+        void setNext(RBX::Primitive*, RBX::Edge*);
+        bool links(RBX::Primitive*, RBX::Primitive*) const;
+        bool links(const RBX::Primitive*) const;
+        virtual void setPrimitive(int32_t, RBX::Primitive*);
+        RBX::Edge& operator=(const RBX::Edge&);
+        virtual void* __vecDelDtor(uint32_t);
+    };
     class EdgeIterator {
     private: 
         RBX::Primitive* primitive;
