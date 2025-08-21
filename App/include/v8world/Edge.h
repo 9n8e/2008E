1645dc9c2d7cd3734dd68660b9d673237787d354
9n8e <boombloxkewl@gmail.com>
2025-08-21 17:52:23 -0400

    added class EdgeIterator

--- App/include/v8world/Edge.h
+++ App/include/v8world/Edge.h
@@ -4,5 +4,23 @@
 
 namespace RBX {
     class Edge : public IPipelined {};
-    class EdgeIterator {};
+    class EdgeIterator {
+    private: 
+        RBX::Primitive* primitive;
+        RBX::Edge* edge;
+        static bool isExternalEdge(RBX::Primitive*, const RBX::Edge*);
+        static RBX::Edge* getNextExternalUtil(RBX::Primitive*, RBX::Edge*);
+        static RBX::Edge* getFirstExternalEdge(RBX::Primitive*);
+        static RBX::Edge* getNextExternalEdge(RBX::Primitive*, RBX::Edge*);
+        void findEdgeOnNextPrimitive();
+
+    public: 
+        EdgeIterator(RBX::Primitive*, RBX::Edge*);
+        RBX::Edge* operator*() const;
+        bool operator==(const RBX::EdgeIterator&) const;
+        bool operator!=(const RBX::EdgeIterator&) const;
+        RBX::EdgeIterator& operator++();
+        static RBX::EdgeIterator begin(RBX::Primitive*);
+        static RBX::EdgeIterator& end();
+    };
 }
