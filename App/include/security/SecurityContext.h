d733ee557705a48e03bee83f16241f105abd7e2f
9n8e <boombloxkewl@gmail.com>
2025-08-30 22:34:20 -0400

    added RBX::Security namespace and children

---
+++ App/include/security/SecurityContext.h
@@ -0,0 +1,31 @@
+namespace RBX {
+    namespace Security {
+        enum Permissions {
+            None = 0x0000,
+            Administrator = 0x0001,
+        };
+
+        enum Identities {
+            Anonymous = 0x0000,
+            LocalGUI = 0x0001,
+            GameScript = 0x0002,
+            CmdLine = 0x0003,
+            TrustedCOM = 0x0004,
+            TrustedWebService = 0x0005,
+            Replicator = 0x0006,
+        };
+
+        class Context {
+        private:
+            const Identities identity;
+            Context(Identities);
+            // tbd // static boost::thread_specific_ptr<Context>& ptr();
+        public:
+            void requirePermisson(Permissions, const char*) const;
+            bool hasPermission(Permissions);
+            
+            static Context& current();
+            static bool isInRole(Identities, Permissions);
+        };
+    }
+}
