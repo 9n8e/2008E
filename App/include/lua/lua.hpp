b2a29b0890b12d418dd0d3c01bd8f059cbcfb93c
9n8e <boombloxkewl@gmail.com>
2025-09-08 22:16:37 -0400

    added lua library

--- App/include/lua/lua.hpp
+++ App/include/lua/lua.hpp
@@ -1,5 +1,15 @@
 namespace RBX {
     namespace Lua {
-        
+        class ScopedPopper {
+        private:
+            int popCount;
+            lua_State* thread;
+
+        public:
+            ScopedPopper(lua_State*, int);
+            ScopedPopper& operator+=(int);
+            ScopedPopper& operator-=(int);
+            ~ScopedPopper();
+        };
     }
 }
