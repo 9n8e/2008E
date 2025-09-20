307a260dbba780b45ec8789b4b6946cfab5a0239
9n8e <boombloxkewl@gmail.com>
2025-09-20 15:56:26 -0400

    added some definitions of StandardOut.cpp before vsc decided to mess around today

--- App/util/StandardOut.cpp
+++ App/util/StandardOut.cpp
@@ -1,16 +1,14 @@
 #include "include/util/StandardOut.h"
 
-namespace RBX {
-    void StandardOut::print(RBX::MessageType type, const char *format, ...) {
-        switch (type) { // RBX::Log
-            case MESSAGE_ERROR:
-            break;
-            case MESSAGE_WARNING:
-            break;
-            case MESSAGE_INFO:
-            break;
-        }
-
-        if (this->l)
+void RBX::StandardOut::print(RBX::MessageType type, const char *format, ...) {
+    switch (type) { // RBX::Log
+        case MESSAGE_ERROR:
+        break;
+        case MESSAGE_WARNING:
+        break;
+        case MESSAGE_INFO:
+        break;
     }
-};
+
+    if ()
+}
