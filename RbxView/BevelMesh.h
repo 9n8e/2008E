b88c59847f49c828e07029494da5a1d46ad4de99
9n8e <boombloxkewl@gmail.com>
2025-09-19 23:13:02 -0400

    init RbxView

---
+++ RbxView/BevelMesh.h
@@ -0,0 +1,9 @@
+namespace RBX::View {
+    class BevelMesh {
+    public:
+        BevelMesh(const RBX::View::BevelMesh&);
+        BevelMesh();
+        virtual ~BevelMesh();
+        RBX::View::BevelMesh& operator=(const RBX::View::BevelMesh&);
+    };
+};
