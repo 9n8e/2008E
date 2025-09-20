dc7b7b8a3a71e74d0521c482cf1fec4f634c525a
9n8e <boombloxkewl@gmail.com>
2025-09-20 15:39:19 -0400

    init RBX::Message

--- App/include/util/StandardOut.h
+++ App/include/util/StandardOut.h
@@ -1,5 +1,24 @@
+#pragma once
+
+#include <exception>
+
+#include <boost/thread/mutex.hpp>
+
 namespace RBX {
     class StandardOut {
-        
+    public: 
+        boost::mutex sync;
+
+        void print(RBX::MessageType, const std::exception&);
+        void print(RBX::MessageType, const char*, ...);
+        StandardOut(const RBX::StandardOut&);
+        StandardOut();
+        virtual ~StandardOut();
+        RBX::StandardOut& operator=(const RBX::StandardOut&);
+        void __local_vftable_ctor_closure();
+        virtual void* __vecDelDtor(int);
+
+        static boost::shared_ptr<RBX::StandardOut> singleton();
+        static void print_exception(const boost::function0<void,std::allocator<boost::function_base> >&, RBX::MessageType, bool);
     };
 };
---
+++ App/include/v8datamodel/Message.h
@@ -0,0 +1,10 @@
+namespace RBX {
+    enum MessageType {
+        MESSAGE_OUTPUT = 0x0000,
+        MESSAGE_INFO = 0x0001,
+        MESSAGE_WARNING = 0x0002,
+        MESSAGE_ERROR = 0x0003,
+    };
+    
+    class Message {};
+}
