2dac595705f04292347c8b8b2315a60a8c54af2d
9n8e <boombloxkewl@gmail.com>
2025-09-20 15:52:10 -0400

    finished RBX::StandardOut definitions

--- App/include/util/StandardOut.h
+++ App/include/util/StandardOut.h
@@ -1,14 +1,34 @@
 #pragma once
 
 #include <exception>
+#include <string>
 
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/function.hpp>
 #include <boost/shared_ptr.hpp>
 
-#include "include/v8datamodel/Message.h"
+#include "v8tree/Service.h"
 
 namespace RBX {
+    enum MessageType {
+        MESSAGE_OUTPUT = 0x0000,
+        MESSAGE_INFO = 0x0001,
+        MESSAGE_WARNING = 0x0002,
+        MESSAGE_ERROR = 0x0003,
+    };
+
+    struct StandardOutMessage {
+    public: 
+        const RBX::MessageType type;
+        const std::string message;
+        int time;
+
+        StandardOutMessage(const RBX::StandardOutMessage&);
+        StandardOutMessage(RBX::MessageType, const char*);
+        ~StandardOutMessage();
+    };
+
     class StandardOut: public boost::enable_shared_from_this<RBX::StandardOut>, public RBX::Notifier<RBX::StandardOut,RBX::StandardOutMessage> {
     public: 
         boost::mutex sync;
