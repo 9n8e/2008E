2c7daf8b3d83d70026b5b652b4a158f57d52d798
9n8e <boombloxkewl@gmail.com>
2025-09-20 19:27:34 -0400

    init RBX::Object && RBX::Creatable

---
+++ App/include/reflection/Object.h
@@ -0,0 +1,24 @@
+namespace RBX {
+    class Object {
+    public: 
+        virtual ~Object();
+        Object(const RBX::Object&);
+        Object();
+        RBX::Object& operator=(const RBX::Object&);
+    };
+
+    template<class F>
+    class Creatable {
+    protected: 
+        Creatable<RBX::Instance>();
+        virtual ~Creatable<RBX::Instance>();
+
+    public:
+        Creatable<RBX::Instance>(const RBX::Creatable<RBX::Instance>&);
+        RBX::Creatable<RBX::Instance>& operator=(const RBX::Creatable<RBX::Instance>&);
+
+    private: 
+        static void* operator new(uint32_t);
+        static void operator delete(void*);
+    };
+}
